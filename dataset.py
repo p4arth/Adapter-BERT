0401cc2cf1fad9bd7cacfc5f8dd2d3bc6552f309
p4arth <paarthbhatnagarh3h3@gmail.com>
2023-04-09 11:54:14 +0530

    added datase 2

---
+++ dataset.py
@@ -0,0 +1,32 @@
+import torch
+import pandas as pd
+from torch.utils.data import Dataset
+
+class ColaDataset(Dataset):
+    def __init__(self, path, tokenizer):
+        self.data = pd.read_csv(path, 
+                                sep = "\t", 
+                                names = ["x", "label", "y", "sentence"],
+                                header = None)[["label", "sentence"]]
+        self.tokenizer = tokenizer
+    
+    def __len__(self):
+        return self.data.shape[0]
+    
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        tokenized_sentence = self.tokenizer.encode_plus(
+            row["sentence"],
+            return_tensors = "pt",
+            padding = "max_length",
+            max_length = 15,
+            truncation = True
+        )
+        label = torch.tensor([row["label"]])
+        return {
+            "labels": label,
+            **tokenized_sentence
+        }
+        
+        
+
